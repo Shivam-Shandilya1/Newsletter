f761ca28ea35f56ce4ab3fcf4917d8c9830c3f7c
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-03-28 11:07:52 +0530

    Use body parse to get the form post

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var express= require ("express");
 var bodyParser = require("body-parser");
 var request = require ("request");
+var https =require("https");
 var app = express();
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/",function(req,res)
 {
-    res.sendFile(__dirname+"/sign-up.html");
+   
+        res.sendFile(__dirname+"/sign-up.html");
+    
+})
+app.post("/",function(req,res)
+{
+    var firstName=req.body.namefirst;
+    var lastName=req.body.namelast;
+    var emailADD=req.body.emailadd;
+    console.log(firstName);
 })
 app.listen(3000,function(){
     console.log("Server is running on port 3000.");
