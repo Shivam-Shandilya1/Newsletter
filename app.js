1f0565ef004bcc0a96fd3e17d43399c0eeb1c323
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-03-31 08:09:04 +0530

    Link to Heroku

--- app.js
+++ app.js
@@ -1,24 +1,68 @@
-var express= require ("express");
-var bodyParser = require("body-parser");
-var request = require ("request");
-var https =require("https");
-var app = express();
+const express= require ("express");
+const bodyParser = require("body-parser");
+const request = require ("request");
+const https =require("https");
+const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res)
 {
-   
         res.sendFile(__dirname+"/sign-up.html");
-    
 })
+
 app.post("/",function(req,res)
 {
-    var firstName=req.body.namefirst;
-    var lastName=req.body.namelast;
-    var emailADD=req.body.emailadd;
-    console.log(firstName);
-})
-app.listen(3000,function(){
+    const firstName=req.body.namefirst;
+    const lastName=req.body.namelast;
+    const emailADD=req.body.emailadd;
+    const data = {
+        members: [
+            {
+                email_address:emailADD,
+                status : "subscribed",
+                merge_fields :
+                {
+                    FNAME: firstName,
+                    LNAME: lastName,
+
+                }
+            }
+        ]
+    };
+    const jsondata = JSON.stringify(data);
+    const url = "https://us1.api.mailchimp.com/3.0/lists/61b19c53e6";
+    const options=
+    {
+        method: "POST",
+        auth:"shivam1:206a87710377b2d1e04ed40310fa3b5e-us1"
+    }
+   const request = https.request(url,options,function(response)
+    {
+        if(response.statusCode === 200)
+        {
+            res.sendFile(__dirname+"/success.html");
+        }else 
+        {
+            res.sendFile(__dirname+"/failure.html")
+        }
+       
+        response.on("data",function(data)
+        {
+            console.log(JSON.parse(data));
+        })
+    
+    });    
+    request.write(jsondata);
+    request.end();
+});
+app.post("/failure",function(req,res)
+{
+res.redirect("/");
+});
+app.listen(process.env.PORT|| 3000,function(){
     console.log("Server is running on port 3000.");
 });
+// API key MailChimp
+// 206a87710377b2d1e04ed40310fa3b5e-us1
+// unique List id 61b19c53e6
